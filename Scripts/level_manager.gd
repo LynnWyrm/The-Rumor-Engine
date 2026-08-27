113af4b47329dba37b82781023ccf123ba8f5899
TheChairGamer <chairgameriscool@gmail.com>
2026-08-27 17:20:32 -0400

    6 new levels

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -8,6 +8,12 @@ var levels := {
 	"level2": "uid://ddlmpjvm1qqjm",
 	"level3": "uid://bsghx1dpixgrk",
 	"level4": "uid://ed3pmag54j3m",
+	"level5": "uid://c7ts6nfyqnx5p",
+	"level6": "uid://dkqdiwn6e3c81",
+	"level7": "uid://cr36eo2x40afr",
+	"level8": "uid://dcft6la4g78d5",
+	"level9": "uid://4wdgagiilbn0",
+	"level10": "uid://1dn4p2s28df2",
 }
 
 func finish_level(people_choosen: Array, level_goal: Array):
@@ -15,6 +21,7 @@ func finish_level(people_choosen: Array, level_goal: Array):
 		print("WIN")
 		current_level += 1
 		if current_level > levels.size():
+			current_level -= 1
 			print("YOU FINISHED")
 		else:
 			get_tree().change_scene_to_file(levels["level" + str(current_level)])
